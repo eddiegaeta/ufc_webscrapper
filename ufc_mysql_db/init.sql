f1b1c970e6593044c69e60db5ecdae011f3438ce
Ed Gaeta <eddiegaeta@gmail.com>
2024-05-27 06:37:21 +0000

    new python image and ect

--- ufc_mysql_db/init.sql
+++ ufc_mysql_db/init.sql
@@ -1,16 +1,15 @@
-
 CREATE DATABASE IF NOT EXISTS mysql01;
 FLUSH PRIVILEGES;
 USE mysql01;
 
-CREATE USER 'mysqluser'@'%' IDENTIFIED BY 'password';
+CREATE USER 'mysqluser'@'%' IDENTIFIED WITH 'caching_sha2_password' BY 'password';
 GRANT ALL PRIVILEGES ON *.* TO 'mysqluser'@'%';
 FLUSH PRIVILEGES;
 
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
+ALTER USER 'root'@'localhost' IDENTIFIED WITH 'caching_sha2_password' BY 'password';
 GRANT ALL ON *.* TO 'root'@'localhost';
 
-ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'password';
+ALTER USER 'root'@'%' IDENTIFIED WITH 'caching_sha2_password' BY 'password';
 GRANT ALL ON *.* TO 'root'@'%';
 
 FLUSH PRIVILEGES;
