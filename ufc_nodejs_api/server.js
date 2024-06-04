5ab27d45d5f811a12d54f3f258066fda66d29a95
Ed Gaeta <eddiegaeta@gmail.com>
2024-06-04 04:27:56 +0000

    updating Docker and server.js

--- ufc_nodejs_api/server.js
+++ ufc_nodejs_api/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
-const mysql = require('mysql2');
 const app = express();
+const mysql = require('mysql2');
 require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
@@ -15,32 +17,26 @@ app.get('/api/events', (req, res) => {
         database: process.env.DB_NAME
     });
 
-    connection.connect((err) => {
-        if (err) {
-            console.error('Error connecting to MySQL:', err);
+    connection.connect();
+
+    connection.query('SELECT * FROM events', function (error, results, fields) {
+        if (error) {
+            console.error('Error executing query:', error);
             res.status(500).send('Internal Server Error');
             return;
         }
 
-        connection.query('SELECT * FROM events', function (error, results, fields) {
-            if (error) {
-                console.error('Error executing query:', error);
-                res.status(500).send('Internal Server Error');
-                return;
-            }
-
-            res.json(results);
-            connection.end();
-        });
+        res.json(results);
     });
+
+    connection.end();
 });
 
-// The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
+// The "catchall" handler: for any request that doesn't match "/api", send back React's index.html file.
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Node is running on port ${PORT}`);
 });
